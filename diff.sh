5590609a79cb814a944167830e6c0b7fca2b92b5
GregChan <gregorywarrenchan@gmail.com>
2017-02-18 12:54:47 -0800

    added labels to diff

--- diff.sh
+++ diff.sh
@@ -3,5 +3,10 @@ MY_PATH="`dirname \"$0\"`"
 VIM_PATH="$MY_PATH/.vimrc"
 BASH_PROFILE_PATH="$MY_PATH/.bash_profile"
 
+echo ".vimrc"
 diff $VIM_PATH ~/.vimrc
+
+echo ""
+
+echo ".bash_profile"
 diff $BASH_PROFILE_PATH ~/.bash_profile
